dccf5304033783762f9274f59d3d2786a51bcc07
Krzysztof Wiktorczyk <rethil@protonmail.com>
2025-10-16 00:42:10 +0200

    system pkg: catppuccin-sddm

--- modules/extra/sddm.nix
+++ modules/extra/sddm.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    catppuccin-sddm
+  ];
+
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
