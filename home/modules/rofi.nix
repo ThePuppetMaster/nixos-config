b4aa6d78415604aaed023e6e1e8cab9fb33754b6
Krzysztof Wiktorczyk <rethil@protonmail.com>
2025-10-15 23:36:07 +0200

    rofi: syntax fix

--- home/modules/rofi.nix
+++ home/modules/rofi.nix
@@ -1,9 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  mkLiteral = lib.formats.rasi.mkLiteral;
+in
 {
   programs.rofi = {
     enable = true;
-    extraConfig = [
+    extraConfig =
       ''
         configuration {
             modi: "run,drun,window";
@@ -20,123 +23,114 @@
             sidebar-mode: true;
         }
       '';
-    ];
 
-    theme = [
-      ''
-        * {
-            bg-col: #1e1e2e;
-            bg-col-light: #1e1e2e;
-            border-col: #89b4fa;
-            selected-col: #1e1e2e;
-            blue: #89b4fa;
-            fg-col: #cdd6f4;
-            fg-col2: #f38ba8;
-            grey: #6c7086;
+    theme = {
+      "*" = {
+        background-color = mkLiteral "#1e1e2e";
+        background-color-light = mkLiteral "#1e1e2e";
+        border-color = mkLiteral "#89b4fa";
+        selected-color = mkLiteral "#1e1e2e";
+        blue = mkLiteral "#89b4fa";
+        foreground-color = mkLiteral "#cdd6f4";
+        foreground-color-2 = mkLiteral "#f38ba8";
+        grey = mkLiteral "#6c7086";
+        width = 600;
+        #font = mkLiteral "JetBrainsMono Nerd Font 14";
+      };
 
-            width: 600;
-            font: "JetBrainsMono Nerd Font 14";
-        }
+      window = {
+        height = 360;
+        border = 2;
+        border-color = mkLiteral "#89b4fa";
+        border-radius = 4;
+        background-color = mkLiteral "#1e1e2e";
+      };
 
-        element-text, element-icon , mode-switcher {
-            background-color: inherit;
-            text-color:       inherit;
-        }
+      mainbox = {
+        background-color = mkLiteral "#1e1e2e";
+      };
 
-        window {
-            height: 360px;
-            border: 2px;
-            border-color: @border-col;
-            border-radius: 4px;
-            background-color: @bg-col;
-        }
+      inputbar = {
+        children = map mkLiteral [ "prompt" "entry" ];
+        background-color = mkLiteral "#1e1e2e";
+        border-radius = mkLiteral "5px";
+        padding = mkLiteral "2px";
+      };
 
-        mainbox {
-            background-color: @bg-col;
-        }
+      prompt = {
+        background-color = mkLiteral "#89b4fa";
+        padding = mkLiteral "6px";
+        text-color = mkLiteral "#1e1e2e";
+        border-radius = mkLiteral "3px";
+        margin = mkLiteral "20px 0px 0px 20px";
+      };
 
-        inputbar {
-            children: [prompt,entry];
-            background-color: @bg-col;
-            border-radius: 5px;
-            padding: 2px;
-        }
+      textbox-prompt-colon = {
+        expand = false;
+        str = mkLiteral ":";
+      };
 
-        prompt {
-            background-color: @blue;
-            padding: 6px;
-            text-color: @bg-col;
-            border-radius: 3px;
-            margin: 20px 0px 0px 20px;
-        }
+      entry = {
+        padding = mkLiteral "6px";
+        margin = mkLiteral "20px 0px 0px 10px";
+        text-color = mkLiteral "#cdd6f4";
+        background-color = mkLiteral "#1e1e2e";
+      };
 
-        textbox-prompt-colon {
-            expand: false;
-            str: ":";
-        }
+      listview = {
+        border = mkLiteral "0px 0px 0px";
+        padding = mkLiteral "6px 0px 0px";
+        margin = mkLiteral "10px 0px 0px 20px";
+        columns = 2;
+        lines = 5;
+        background-color = mkLiteral "#1e1e2e";
+      };
 
-        entry {
-            padding: 6px;
-            margin: 20px 0px 0px 10px;
-            text-color: @fg-col;
-            background-color: @bg-col;
-        }
+      element = {
+        padding = mkLiteral "5px";
+        background-color = mkLiteral "#1e1e2e";
+        text-color = mkLiteral "#cdd6f4";
+      };
 
-        listview {
-            border: 0px 0px 0px;
-            padding: 6px 0px 0px;
-            margin: 10px 0px 0px 20px;
-            columns: 2;
-            lines: 5;
-            background-color: @bg-col;
-        }
+      element-icon = {
+        size = mkLiteral "25px";
+      };
 
-        element {
-            padding: 5px;
-            background-color: @bg-col;
-            text-color: @fg-col  ;
-        }
+      "element selected" = {
+        background-color = mkLiteral "#1e1e2e";
+        text-color = mkLiteral "#f38ba8";
+      };
 
-        element-icon {
-            size: 25px;
-        }
+      mode-switcher = {
+        spacing = 0;
+      };
 
-        element selected {
-            background-color:  @selected-col ;
-            text-color: @fg-col2  ;
-        }
+      button = {
+        padding = mkLiteral "10px";
+        background-color = mkLiteral "#1e1e2e";
+        text-color = mkLiteral "#6c7086";
+        vertical-align = mkLiteral "0.5";
+        horizontal-align = mkLiteral "0.5";
+      };
 
-        mode-switcher {
-            spacing: 0;
-          }
+      "button selected" = {
+        background-color = mkLiteral "#1e1e2e";
+        text-color = mkLiteral "#89b4fa";
+      };
 
-        button {
-            padding: 10px;
-            background-color: @bg-col-light;
-            text-color: @grey;
-            vertical-align: 0.5;
-            horizontal-align: 0.5;
-        }
+      message = {
+        background-color = mkLiteral "#1e1e2e";
+        margin = mkLiteral "2px";
+        padding = mkLiteral "2px";
+        border-radius = mkLiteral "5px";
+      };
 
-        button selected {
-          background-color: @bg-col;
-          text-color: @blue;
-        }
-
-        message {
-            background-color: @bg-col-light;
-            margin: 2px;
-            padding: 2px;
-            border-radius: 5px;
-        }
-
-        textbox {
-            padding: 6px;
-            margin: 20px 0px 0px 20px;
-            text-color: @blue;
-            background-color: @bg-col-light;
-        }
-      '';
-    ];
+      textbox = {
+        padding = mkLiteral "6px";
+        margin = mkLiteral "20px 0px 0px 20px";
+        text-color = mkLiteral "#89b4fa";
+        background-color = mkLiteral "#1e1e2e";
+      };
+    };
   };
 }
