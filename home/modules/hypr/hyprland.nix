71bb075daa4aad19066536a6d5eb6cbb278bdd28
Krzysztof Wiktorczyk <rethil@protonmail.com>
2025-10-17 12:16:11 +0200

    hyprland: env

--- home/modules/hypr/hyprland.nix
+++ home/modules/hypr/hyprland.nix
@@ -11,8 +11,25 @@
     settings = {
       # Environment variables.
       env = [
+        # Cursor.
         "XCURSOR_SIZE,24"
         "HYPRCURSOR_SIZE,24"
+
+        # Toolkits.
+        "GDK_BACKEND,wayland,x11,*"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "SDL_VIDEODRIVER,wayland"
+        "CLUTTER_BACKEND,wayland"
+
+        # XDG.
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+
+        # Qt.
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
       ];
 
       # Autostart.
